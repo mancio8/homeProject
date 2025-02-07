f0bd5de81f1d84984d54ffbfaf68159e0ddcdd11
mancio8 <mancinelliasr8@gmail.com>
2025-02-07 21:35:15 +0100

    add index

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
     path('f1/', include('f1.urls')),
+    path('', views.index, name='index'),
 ]
